47989ad99742f5ee6dfd53ed4e62e51b0c8a0761
raghad al shaer <raghadalshaer25@gmail.com>
2024-12-17 11:05:42 +0300

    init commit

---
+++ i_am_just_a_cake/i_am_just_a_cake/src/i_am_just_a_cake/I_am_just_a_cake.java
@@ -0,0 +1,16 @@
+package i_am_just_a_cake;
+
+import java.awt.Color;
+
+public class I_am_just_a_cake {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        login loginWindows = new login(); 
+        loginWindows.show();
+        Color babyPink = new Color(255,218,230);
+        loginWindows.getContentPane().setBackground(babyPink);
+    }
+    
+}
